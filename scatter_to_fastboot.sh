067f08025d5d9173650c0dd49ce06c1366aff207
RiProG-id <81682107+RiProG-id@users.noreply.github.com>
2024-06-11 18:33:25 +0800

    Update scatter_to_fastboot.sh
    Fix partition NONE

--- scatter_to_fastboot.sh
+++ scatter_to_fastboot.sh
@@ -15,27 +15,20 @@ echo "$dummy1" > "$scatterdir"/flash_all.sh
 echo "$dummy1" > "$scatterdir"/flash_all_except_data_storage.sh
 while IFS= read -r line || [ -n "$line" ]; do
   if [ -n "$partition_index" ] && [ -n "$partition_name" ] && [ -n "$file_name" ] && [ -n "$is_download" ] && [ -n "$is_upgradable" ]; then
-  echo "Processing $partition_index"
     if [ "$is_download" = "true" ]; then
-      if [ "$partition_name" = metadata ]; then
-        metadata=true
-      fi
-      if [ "$partition_name" = md_udc ]; then
-        md_udc=true
-      fi
-    echo "fastboot flash $partition_name $file_name" >> "$scatterdir"/flash_all.sh
+      echo "Processing $partition_index"
+      echo "fastboot flash $partition_name $file_name" >> "$scatterdir"/flash_all.sh
       if [ "$is_upgradable" = "true" ]; then
         echo "fastboot flash $partition_name $file_name" >> "$scatterdir"/flash_all_except_data_storage.sh
       fi
-    if [ "$partition_name" = boot_a ]; then
-      boot_a=true
-    fi 
-    fi 
-  partition_index=""
-  partition_name=""
-  file_name=""
-  is_download=""
-  is_upgradable=""
+    else
+    echo "Skipping $partition_index" 
+    fi
+    partition_index=""
+    partition_name=""
+    file_name=""
+    is_download=""
+    is_upgradable=""
  else
     if echo "$line" | grep -q partition_index; then
       partition_index=$(echo "$line" | awk '{print $3}')
@@ -54,16 +47,15 @@ while IFS= read -r line || [ -n "$line" ]; do
     fi
   fi
 done < "$scatter"
-  
-if [ "$metadata" = "true" ]; then
-  echo "fastboot erase metadata" >> "$scatterdir"/flash_all_except_data_storage.sh
-  fi
-if [ "$md_udc" = "true" ]; then
-  echo "fastboot erase md_udc" >> "$scatterdir"/flash_all_except_data_storage.sh
+if grep -q metadata "$scatter"; then
+  echo "fastboot erase metadata" >> "$scatterdir/flash_all.sh"
 fi
-if [ "$boot_a" = "true" ]; then
-  echo "fastboot --set-active=a" >> "$scatterdir"/flash_all.sh
-  echo "fastboot --set-active=a" >> "$scatterdir"/flash_all_except_data_storage.sh
+if grep -q md_udc "$scatter"; then
+  echo "fastboot erase md_udc" >> "$scatterdir/flash_all.sh"
+fi
+if grep -q -e boot_a -e boot_b "$scatter"; then
+  echo "fastboot --set-active=a" >> "$scatterdir/flash_all.sh"
+  echo "fastboot --set-active=a" >> "$scatterdir/flash_all_except_data_storage.sh"
 fi
 dummy2='fastboot oem cdms
 fastboot reboot
